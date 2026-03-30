f6b6b190294f6cff52477a9186e221dbe23c7907
NoBirds@Laptop <wuqiufeng1417469@gmail.com>
2026-03-30 14:22:28 +0800

    Add Module: config.py for handling .ini files.

---
+++ common/config.py
@@ -0,0 +1,23 @@
+import configparser
+from os.path import exists, isfile
+from os import getcwd
+
+
+CONFIGS = None
+class Config():
+    _config = configparser.ConfigParser()
+    
+    @staticmethod
+    def add_conf(path: str = 'config.ini'):
+        if not exists(path):
+                raise FileNotFoundError(f'{path} does not exist in {getcwd()}')
+        if not isfile(path):
+            raise IsADirectoryError(f'{path} is not a file.')
+  
+        Config._config.read(path)
+    
+    @staticmethod
+    def get() -> configparser.ConfigParser:
+        if len(Config._config.keys()) <= 0:
+            Config.add_conf()
+        return Config._config
