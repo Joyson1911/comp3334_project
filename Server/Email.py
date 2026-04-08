86e64a1aaed7bfb3f748fbe01dfaf2c0bcefa359
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2026-04-08 19:13:55 +0800

    Fix: Email verification can now be sent functionally.

--- Server/Email.py
+++ Server/Email.py
@@ -1,19 +1,24 @@
 import smtplib
 from config import Config
+from email.utils import formataddr
+from email.message import EmailMessage
 
-
-
-def emailVerification(verCode: int, receiverMailAddress: str):
+def emailVerification(verCode: str, receiverMailAddress: str):
     Conf = Config.get()
     ADDR = Conf['EMAIL']['address']
     PW = Conf['EMAIL']['password']
-    message = "Verification Code: " + str(verCode)
+    message = EmailMessage()
+    message['Subject'] = "Your Happy Chat Verification Code"
+    message["From"] = formataddr(("Happy Chat", ADDR))
+    message['To'] = receiverMailAddress
+    message.set_content(f'Your Happy chat code is {verCode}.')
+    
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(ADDR, PW)
-        server.sendmail(ADDR, receiverMailAddress, message)
+        server.send_message(message, ADDR, receiverMailAddress)
     except Exception as e:
-        print("Error: failed to send verification code")
+        print(f"Error: failed to send verification code:\n{e}")
     finally:
         server.quit()
